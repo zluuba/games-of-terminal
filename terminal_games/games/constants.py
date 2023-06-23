ea3c17ec785701fa9b57d48ea30f9c25ecc35a1a
zluuba <zluyba.nikitina@gmail.com>
2023-06-23 15:14:24 +0300

    feat: create file for constants from menu

---
+++ terminal_games/games/constants.py
@@ -0,0 +1,7 @@
+MENU = ['Snake', 'Tic Tac Toe', 'Minesweeper', 'Tetris', 'Unsolicited advice']
+GOODBYE_WORDS = [
+    'Already miss you.', 'Where are you going?',
+    'Don\'t you go. ˙◠˙', 'Bye.', 'Have a great day!',
+    'Nice.', 'Finally.', 'We had a great time!',
+    'Shall we do it again?', 'Don\'t forget to rest.', 'Huh.',
+]
