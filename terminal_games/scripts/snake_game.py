c45c1b0741b728be193c8eb6b7a54a2660ba6914
zluuba <zluyba.nikitina@gmail.com>
2023-06-23 15:12:12 +0300

    fix: rebuild scripts logic - now start from menu

--- terminal_games/scripts/snake_game.py
+++ terminal_games/scripts/snake_game.py
@@ -1,12 +1,6 @@
 from terminal_games.games.snake.core import SnakeGame
-import curses
 
 
-@curses.wrapper
-def main(canvas):
+def start_snake_game(canvas):
     game = SnakeGame(canvas)
     game.start_new_game()
-
-
-if __name__ == '__main__':
-    main()
--- terminal_games/scripts/tictactoe_game.py
+++ terminal_games/scripts/tictactoe_game.py
@@ -1,12 +1,6 @@
 from terminal_games.games.tictactoe.core import TicTacToeGame
-import curses
 
 
-@curses.wrapper
-def main(canvas):
+def start_tictactoe_game(canvas):
     game = TicTacToeGame(canvas)
     game.start_new_game()
-
-
-if __name__ == '__main__':
-    main()
