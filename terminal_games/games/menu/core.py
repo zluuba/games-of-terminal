ec152f90283b1e63890fc8ae5faa7536b8cfee3f
zluuba <zluyba.nikitina@gmail.com>
2023-07-10 08:55:09 +0300

    fix: update menu

--- terminal_games/games/menu/core.py
+++ terminal_games/games/menu/core.py
@@ -1,5 +1,7 @@
 from terminal_games.scripts.tictactoe_game import start_tictactoe_game
 from terminal_games.scripts.snake_game import start_snake_game
+from terminal_games.scripts.minesweeper_game import start_minesweeper_game
+from terminal_games.scripts.tetris_game import start_tetris_game
 from terminal_games.games.menu.constants import *
 
 import curses
@@ -35,7 +37,6 @@ class Menu:
         x = w // 2
         y = h // 2
 
-        # set background
         self.canvas.bkgd(' ', curses.color_pair(1))
 
         menu_current_row_ind = 0
@@ -60,15 +61,12 @@ class Menu:
             elif key in (curses.KEY_ENTER, 10, 13):
                 if menu_current_row_ind == 0:
                     start_snake_game(self.canvas)
+                elif menu_current_row_ind == 1:
+                    start_minesweeper_game(self.canvas)
                 elif menu_current_row_ind == 2:
                     start_tictactoe_game(self.canvas)
-                else:
-                    self.canvas.addstr(
-                        y, x - len(MENU[menu_current_row_ind]) // 2,
-                        MENU[menu_current_row_ind]
-                    )
-                    self.canvas.refresh()
-                    self.canvas.getch()
+                elif menu_current_row_ind == 3:
+                    start_tetris_game(self.canvas)
 
             self.show_menu(self.canvas, menu_current_row_ind)
 
