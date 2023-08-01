38e9e2c79382950d02d67b5771b77551adafc2b2
zluuba <zluyba.nikitina@gmail.com>
2023-08-01 16:37:55 +0300

    fix: add rules, menu tips

--- terminal_games/games/minesweeper/constants.py
+++ terminal_games/games/minesweeper/constants.py
@@ -1,10 +1,26 @@
 import curses
 
 
+RULES = (
+    'Minesweeper is a game where mines are hidden in a grid of squares. '
+    'Safe squares have numbers telling you how many mines touch the square. '
+    'You can use the number clues to solve the game by opening all of the safe squares. '
+    'If you click on a mine you lose the game.'
+)
+
+SIDE_MENU_TIPS = [
+    'Rules     - r',
+    'Move      - ← ↓ ↑ →',
+    'Quit      - q',
+    'Pause     - p',
+    'Hide tips - h',
+]
+
 DIRECTIONS = {
     curses.KEY_RIGHT: (0, 1), curses.KEY_LEFT: (0, -1),
     curses.KEY_UP: (-1, 0), curses.KEY_DOWN: (1, 0),
 }
+
 FIELD = [[1, 2, 3, 4, 5],
          [6, 7, 8, 9, 10],
          [11, 12, 13, 14, 15],
