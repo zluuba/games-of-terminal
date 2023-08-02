040b6e10c41cac9f08b8a5747dfe03828bcd22e1
zluuba <zluyba.nikitina@gmail.com>
2023-08-02 16:28:02 +0300

    fix: rebuild draw boxes logic, sizes

--- terminal_games/games/engine.py
+++ terminal_games/games/engine.py
@@ -1,40 +1,13 @@
 import curses
 
 
-"""
-SIZES:
-
-logo:
-    width: 31  (fixed)
-    height: 5  (fixed)
-
-logo_box:
-    lines: 7   (fixed)
-    cols: 35  (fixed)
-    begin_y: 1                    (fixed)
-    begin_x: screen_width - cols  (float)
-
-side_menu_box:
-    lines: logo_box.begin_y + logo_box.lines  (fixed)
-    cols: 35  (fixed)
-    begin_y: logo_box.begin_y + logo_box.lines  (fixed)
-    begin_x: logo_box.begin_x                   (fixed)
-
-game_box:
-    lines: height - 2  (float)
-    cols: width - 2    (float)
-    begin_y: 1  (fixed)
-    begin_x: 1  (fixed)
-"""
-
-
-LOGO = """
-######  #####  #####   ##   ##
-  ##    ##     ##  ##  ### ###
-  ##    ####   ####    ## # ##
-  ##    ##     ## ##   ##   ##
-  ##    #####  ##  ##  ##   ##
-"""
+LOGO = [
+    '######  #####  #####   ##   ##',
+    '  ##    ##     ##  ##  ### ###',
+    '  ##    ####   ####    ## # ##',
+    '  ##    ##     ## ##   ##   ##',
+    '  ##    #####  ##  ##  ##   ##',
+]
 
 
 class GameEngine:
@@ -42,7 +15,8 @@ class GameEngine:
         self.canvas = canvas
         self._setup()
 
-    def _init_colors(self):
+    @staticmethod
+    def _init_colors():
         curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)     # black
         curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_GREEN)     # green
         curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_RED)       # red
@@ -58,72 +32,62 @@ class GameEngine:
         self._init_colors()
         self._setup_sizes()
 
-        self._draw_window_box()
-        self._draw_game_box()
-        self._draw_side_menu_box()
-        self._draw_logo_box()
+        self._draw_window()
+        self._setup_boxes()
         self._draw_logo()
 
-    def _setup_sizes(self):
-        self.side_menu_box_sizes = {
-            'lines': self.height - 8,
-            'cols': 34,
-            'begin_y': 7,
-            'begin_x': self.width - 34,
-        }
-        self.logo_box_sizes = {
-            'lines': 7,
-            'cols': 34,
-            'begin_y': 1,
-            'begin_x': self.width - 34,
-        }
-        self.game_box_sizes = {
-            'lines': self.height - 2,
-            'cols': self.width - 34,
-            'begin_y': 1,
-            'begin_x': 0,
-        }
-        self.window_box_sizes = {
-            'lines': self.height,
-            'cols': self.width,
-            'begin_y': 0,
-            'begin_x': 0,
-        }
+    def _setup_boxes(self):
+        self.game_box = self._draw_box(self.sizes['game_box'])
+        self.side_menu_box = self._draw_box(self.sizes['side_menu_box'])
+        self.logo_box = self._draw_box(self.sizes['logo_box'])
 
-    def _draw_window_box(self):
-        box_sizes = self.window_box_sizes.values()
-        self.window = curses.newwin(*box_sizes)
+    def _draw_box(self, sizes):
+        box = self.window.subwin(*sizes.values())
+        box.border()
+        return box
+
+    def _draw_window(self):
+        sizes = self.sizes['window_box'].values()
+        self.window = curses.newwin(*sizes)
         self.window.nodelay(True)
         self.window.keypad(True)
-
-    def _draw_game_box(self):
-        box_sizes = self.game_box_sizes.values()
-        self.game_box = self.window.subwin(*box_sizes)
-        self.game_box.border()
-
-    def _draw_side_menu_box(self):
-        box_sizes = self.side_menu_box_sizes.values()
-        self.side_menu_box = self.window.subwin(*box_sizes)
-        self.side_menu_box.border()
-
-    def _draw_logo_box(self):
-        box_sizes = self.logo_box_sizes.values()
-        self.logo_box = self.window.subwin(*box_sizes)
-        self.logo_box.border()
 
     def _draw_logo(self):
         y, x = 1, 2
 
-        for line in LOGO.strip('\n').split('\n'):
+        for line in LOGO:
             self.logo_box.addstr(y, x, line)
             y += 1
 
         self.logo_box.addstr(6, 14, ' GAMES ')
 
     def _wait(self):
-        """
-        Using with getch() function,
-        waiting for the user to press a key,
-        do nothing at this moment
-        """
         self.window.timeout(-1)
+
+    def _setup_sizes(self):
+        self.sizes = {
+            'game_box': {
+                'lines': self.height - 2,
+                'cols': self.width - 34,
+                'begin_y': 1,
+                'begin_x': 0,
+            },
+            'side_menu_box': {
+                'lines': self.height - 8,
+                'cols': 34,
+                'begin_y': 7,
+                'begin_x': self.width - 34,
+            },
+            'logo_box': {
+                'lines': 7,
+                'cols': 34,
+                'begin_y': 1,
+                'begin_x': self.width - 34,
+            },
+            'window_box': {
+                'lines': self.height,
+                'cols': self.width,
+                'begin_y': 0,
+                'begin_x': 0,
+            },
+        }
