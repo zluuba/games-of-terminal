19344af513742715f4226feff8c36bc2886a619c
zluuba <zluyba.nikitina@gmail.com>
2023-10-16 10:54:29 +0300

    fix: rebuild menu, make it more clean

--- terminal_games/games/menu/core.py
+++ terminal_games/games/menu/core.py
@@ -1,7 +1,3 @@
-from terminal_games.scripts.tictactoe_game import start_tictactoe_game
-from terminal_games.scripts.snake_game import start_snake_game
-from terminal_games.scripts.minesweeper_game import start_minesweeper_game
-from terminal_games.scripts.tetris_game import start_tetris_game
 from terminal_games.games.menu.constants import *
 
 import curses
@@ -13,34 +9,36 @@ import sys
 class Menu:
     def __init__(self, canvas):
         self.canvas = canvas
+        self._setup()
 
-    def show_menu(self, stdscr, selected_row_ind):
-        stdscr.clear()
-        h, w = stdscr.getmaxyx()
+    def _setup(self):
+        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
-        for ind, item in enumerate(MENU):
-            x = w // 2 - len(item) // 2
-            y = h // 2 - len(MENU) // 2 + ind
+        self.canvas.bkgd(' ', curses.color_pair(1))
+        self.height, self.width = self.canvas.getmaxyx()
 
-            if ind == selected_row_ind:
-                stdscr.addstr(y, x, item, curses.A_STANDOUT)
+        self.curr_row = 1
+        self.menu_length = len(GAMES)
+
+    def _show_menu(self):
+        self.canvas.clear()
+
+        for row, item in enumerate(GAMES.values(), start=1):
+            game_name = item['name']
+            x = self.width // 2 - len(game_name) // 2
+            y = self.height // 2 - self.menu_length // 2 + row
+
+            if row == self.curr_row:
+                self.canvas.addstr(y, x, game_name, curses.A_STANDOUT)
             else:
-                stdscr.addstr(y, x, item)
+                self.canvas.addstr(y, x, game_name)
 
-        stdscr.refresh()
+        self.canvas.refresh()
 
     def main(self):
         curses.curs_set(0)
-        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
-
-        h, w = self.canvas.getmaxyx()
-        x = w // 2
-        y = h // 2
-
-        self.canvas.bkgd(' ', curses.color_pair(1))
-
-        menu_current_row_ind = 0
-        self.show_menu(self.canvas, menu_current_row_ind)
+        self._change_the_game()
+        self._show_menu()
 
         while True:
             key = self.canvas.getch()
@@ -48,26 +46,30 @@ class Menu:
 
             # 27 - Esc button
             if key == 27:
-                goodbye_message = random.choice(GOODBYE_WORDS)
-                self.canvas.addstr(y, x - len(goodbye_message) // 2, goodbye_message)
-                self.canvas.refresh()
-                time.sleep(1.5)
-                sys.exit(0)
-
-            if key == curses.KEY_UP and menu_current_row_ind > 0:
-                menu_current_row_ind -= 1
-            elif key == curses.KEY_DOWN and menu_current_row_ind < len(MENU) - 1:
-                menu_current_row_ind += 1
+                self._exit()
+            elif key == curses.KEY_UP and self.curr_row > 1:
+                self.curr_row -= 1
+            elif key == curses.KEY_DOWN and self.curr_row < self.menu_length:
+                self.curr_row += 1
             elif key in (curses.KEY_ENTER, 10, 13):
-                if menu_current_row_ind == 0:
-                    start_snake_game(self.canvas)
-                elif menu_current_row_ind == 1:
-                    start_minesweeper_game(self.canvas)
-                elif menu_current_row_ind == 2:
-                    start_tictactoe_game(self.canvas)
-                elif menu_current_row_ind == 3:
-                    start_tetris_game(self.canvas)
+                self.game.start_new_game()
 
-            self.show_menu(self.canvas, menu_current_row_ind)
-
+            self._change_the_game()
+            self._show_menu()
             self.canvas.refresh()
+
+    def _change_the_game(self):
+        current_game = GAMES[self.curr_row]['game']
+        self.game = current_game(self.canvas)
+
+    def _exit(self):
+        goodbye_message = random.choice(GOODBYE_MESSAGES)
+        self.canvas.addstr(
+            self.height // 2,
+            (self.width // 2) - len(goodbye_message) // 2,
+            goodbye_message,
+        )
+        self.canvas.refresh()
+
+        time.sleep(1)
+        sys.exit(0)
