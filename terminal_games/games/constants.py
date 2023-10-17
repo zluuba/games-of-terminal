63a31ca993d92e38a4e29ca44b927546da6ddd17
zluuba <zluyba.nikitina@gmail.com>
2023-10-17 16:14:34 +0300

    fix: update constants - add MESSAGES

--- terminal_games/games/constants.py
+++ terminal_games/games/constants.py
@@ -5,3 +5,12 @@ LOGO = [
     '  ##    ##     ## ##   ##   ##',
     '  ##    #####  ##  ##  ##   ##',
 ]
+
+MESSAGES = {
+    'game_over': 'GAME OVER',
+    'score_text': 'Score: ',
+    'best_score': 'Best score: ',
+    'new_best_score': 'New best score!',
+    'start_text': 'Press any key to start',
+    'play_again': 'Press Space to play again',
+}
