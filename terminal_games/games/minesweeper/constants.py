818541ef93117fe1e011735145a79c6c47bf3566
zluuba <zluyba.nikitina@gmail.com>
2023-10-23 13:47:56 +0300

    fix(minesweeper): add cursor control keys - WASD

--- terminal_games/games/minesweeper/constants.py
+++ terminal_games/games/minesweeper/constants.py
@@ -9,8 +9,10 @@ RULES = (
 )
 
 DIRECTIONS = {
-    curses.KEY_RIGHT: (0, 7), curses.KEY_LEFT: (0, -7),
+    curses.KEY_LEFT: (0, -7), curses.KEY_RIGHT: (0, 7),
     curses.KEY_UP: (-3, 0), curses.KEY_DOWN: (3, 0),
+    ord('a'): (0, -7), ord('d'): (0, 7),
+    ord('w'): (-3, 0), ord('s'): (3, 0),
 }
 
 CELL_OFFSETS = ((-3, -7), (-3, 0), (-3, 7),
