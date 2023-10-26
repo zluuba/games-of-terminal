b4432444696452922df79a349113356ef91a5fcc
zluuba <zluyba.nikitina@gmail.com>
2023-10-26 17:26:23 +0300

    refactor(menu): update func names, split _exit func

--- games_of_terminal/games/menu/constants.py
+++ games_of_terminal/games/menu/constants.py
@@ -5,10 +5,10 @@ from games_of_terminal.games.tetris.core import TetrisGame
 
 
 GAMES = {
-    1: {'name': 'Snake', 'game': SnakeGame},
-    2: {'name': 'Minesweeper', 'game': MinesweeperGame},
-    3: {'name': 'Tic Tac Toe', 'game': TicTacToeGame},
-    4: {'name': 'Tetris', 'game': TetrisGame},
+    0: {'name': 'Snake', 'game': SnakeGame},
+    1: {'name': 'Minesweeper', 'game': MinesweeperGame},
+    2: {'name': 'Tic Tac Toe', 'game': TicTacToeGame},
+    3: {'name': 'Tetris', 'game': TetrisGame},
 }
 
 GOODBYE_MESSAGES = [
--- games_of_terminal/games/menu/core.py
+++ games_of_terminal/games/menu/core.py
@@ -1,6 +1,6 @@
 from games_of_terminal.games.menu.constants import GAMES, GOODBYE_MESSAGES
 from games_of_terminal.games.constants import KEYS
-from games_of_terminal.games.engine import Engine
+from games_of_terminal.games.app_interface import AppInterfaceManager
 
 from random import choice
 import curses
@@ -8,62 +8,62 @@ import time
 import sys
 
 
-class Menu(Engine):
+class Menu(AppInterfaceManager):
     def _setup(self):
         super()._setup()
 
-        self.curr_row = 1
+        self.current_row = 0
         self.menu_length = len(GAMES)
 
-    def _show_menu(self):
-        color = self.get_color_by_name('white_text_black_bg')
-        reversed_color = color + curses.A_STANDOUT
-        self.window.clear()
-
-        for row, item in enumerate(GAMES.values(), start=1):
-            game_name = item['name']
-            x = self.width // 2 - len(game_name) // 2
-            y = self.height // 2 - self.menu_length // 2 + row
-
-            if row == self.curr_row:
-                self.window.addstr(y, x, game_name, reversed_color)
-            else:
-                self.window.addstr(y, x, game_name, color)
-
-        self.window.refresh()
-
-    def main(self):
-        curses.curs_set(0)
-        self._show_menu()
+    def run_menu_loop(self):
+        self.hide_cursor()
 
         while True:
             key = self.window.getch()
+            self.wait_for_keypress()
 
             if key == KEYS['escape']:
                 self._exit()
-            elif key == KEYS['up_arrow'] and self.curr_row > 1:
-                self.curr_row -= 1
-            elif key == KEYS['down_arrow'] and self.curr_row < self.menu_length:
-                self.curr_row += 1
+            elif key == KEYS['up_arrow']:
+                self.current_row -= 1 if self.current_row > 0 else 0
+            elif key == KEYS['down_arrow']:
+                self.current_row += 1 if self.current_row < self.menu_length - 1 else 0
             elif key in KEYS['enter']:
-                chosen_game = GAMES[self.curr_row]['game']
+                chosen_game = GAMES[self.current_row]['game']
                 new_game = chosen_game(self.window)
                 new_game.start_new_game()
 
             self._show_menu()
             self.window.refresh()
 
-    def _exit(self):
+    def _show_menu(self):
+        item_color = self.get_color_by_name('white_text_black_bg')
+        selected_item_color = item_color + curses.A_STANDOUT
         self.window.clear()
 
-        goodbye_message = choice(GOODBYE_MESSAGES)
-        self.window.addstr(
-            self.height // 2,
-            (self.width // 2) - len(goodbye_message) // 2,
-            goodbye_message,
-            self.get_color_by_name('white_text_black_bg'),
-        )
+        for row, item in enumerate(GAMES.values()):
+            game_name = item['name']
+            x = self.width // 2 - len(game_name) // 2
+            y = self.height // 2 - self.menu_length // 2 + row
+
+            if row == self.current_row:
+                self.window.addstr(y, x, game_name, selected_item_color)
+            else:
+                self.window.addstr(y, x, game_name, item_color)
+
         self.window.refresh()
 
+    def _exit(self):
+        self.window.clear()
+        self._draw_goodbye_message()
         time.sleep(1)
         sys.exit(0)
+
+    def _draw_goodbye_message(self):
+        goodbye_message = choice(GOODBYE_MESSAGES)
+        color = self.get_color_by_name('white_text_black_bg')
+
+        begin_y = self.height // 2
+        begin_x = (self.width // 2) - len(goodbye_message) // 2
+
+        self.draw_message(begin_y, begin_x, self.window, goodbye_message, color)
