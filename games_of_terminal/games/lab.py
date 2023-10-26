ea2eb50d3e32c2098caa0002fa561446b291c2ad
zluuba <zluyba.nikitina@gmail.com>
2023-10-26 17:19:04 +0300

    fix: move lab into games module

--- games_of_terminal/games/lab.py
+++ games_of_terminal/games/lab.py
@@ -1,7 +1,44 @@
+""" for experiments """
+
 import curses
+import sys
 
 
-def main(stdscr):
+def main(canvas):
+    curses.curs_set(0)
+    curses.mousemask(1)
+
+    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_GREEN)
+    curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_RED)
+
+    # height, width = canvas.getmaxyx()
+
+    # nlines, ncols, begin_y, begin_x
+    window = curses.newwin(10, 15, 2, 2)
+    window.nodelay(True)
+    window.keypad(True)
+    window.border()
+
+    while True:
+        key = window.getch()
+
+        if key == curses.KEY_MOUSE:
+            _, x, y, _, _ = curses.getmouse()
+
+            if 2 <= x <= 17 and 2 <= y <= 12:
+                window.addstr(2, 2, f'x: {x}, y: {y}', curses.color_pair(1))
+            else:
+                window.addstr(2, 2, f'x: {x}, y: {y}', curses.color_pair(2))
+
+        if key == 27:
+            sys.exit(0)
+
+
+# curses.wrapper(main)
+
+
+# click on terminal field experimental function
+def click(stdscr):
     curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_RED)
     curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_GREEN)
 
@@ -37,4 +74,4 @@ def main(stdscr):
             break
 
 
-curses.wrapper(main)
+# curses.wrapper(click)
