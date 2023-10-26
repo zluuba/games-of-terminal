d4e746a959e4059ded82a0c68257c9319ebd2294
zluuba <zluyba.nikitina@gmail.com>
2023-10-26 12:49:40 +0300

    refactor(menu): add Engine class inheritance, minor changes

--- games_of_terminal/games/menu/core.py
+++ games_of_terminal/games/menu/core.py
@@ -1,32 +1,24 @@
-from games_of_terminal.games.menu.constants import *
+from games_of_terminal.games.menu.constants import GAMES, GOODBYE_MESSAGES
 from games_of_terminal.games.constants import KEYS
+from games_of_terminal.games.engine import Engine
 
+from random import choice
 import curses
-import random
 import time
 import sys
 
 
-class Menu:
-    def __init__(self, canvas):
-        # TODO:
-        #  - draw window instead of raw canvas
-        #  - do I need to use GameEngine class there? For initial setup
-
-        self.canvas = canvas
-        self._setup()
-
+class Menu(Engine):
     def _setup(self):
-        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
-
-        self.canvas.bkgd(' ', curses.color_pair(1))
-        self.height, self.width = self.canvas.getmaxyx()
+        super()._setup()
 
         self.curr_row = 1
         self.menu_length = len(GAMES)
 
     def _show_menu(self):
-        self.canvas.clear()
+        color = self.get_color_by_name('white_text_black_bg')
+        reversed_color = color + curses.A_STANDOUT
+        self.window.clear()
 
         for row, item in enumerate(GAMES.values(), start=1):
             game_name = item['name']
@@ -34,19 +26,18 @@ class Menu:
             y = self.height // 2 - self.menu_length // 2 + row
 
             if row == self.curr_row:
-                self.canvas.addstr(y, x, game_name, curses.A_STANDOUT)
+                self.window.addstr(y, x, game_name, reversed_color)
             else:
-                self.canvas.addstr(y, x, game_name)
+                self.window.addstr(y, x, game_name, color)
 
-        self.canvas.refresh()
+        self.window.refresh()
 
     def main(self):
         curses.curs_set(0)
         self._show_menu()
 
         while True:
-            key = self.canvas.getch()
-            self.canvas.clear()
+            key = self.window.getch()
 
             if key == KEYS['escape']:
                 self._exit()
@@ -56,20 +47,23 @@ class Menu:
                 self.curr_row += 1
             elif key in KEYS['enter']:
                 chosen_game = GAMES[self.curr_row]['game']
-                new_game = chosen_game(self.canvas)
+                new_game = chosen_game(self.window)
                 new_game.start_new_game()
 
             self._show_menu()
-            self.canvas.refresh()
+            self.window.refresh()
 
     def _exit(self):
-        goodbye_message = random.choice(GOODBYE_MESSAGES)
-        self.canvas.addstr(
+        self.window.clear()
+
+        goodbye_message = choice(GOODBYE_MESSAGES)
+        self.window.addstr(
             self.height // 2,
             (self.width // 2) - len(goodbye_message) // 2,
             goodbye_message,
+            self.get_color_by_name('white_text_black_bg'),
         )
-        self.canvas.refresh()
+        self.window.refresh()
 
         time.sleep(1)
         sys.exit(0)
