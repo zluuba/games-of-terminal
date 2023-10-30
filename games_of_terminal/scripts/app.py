1c438f78bb05771c4e94b33029f43a1f36f6b7d4
zluuba <zluyba.nikitina@gmail.com>
2023-10-30 16:38:35 +0300

    feat(scripts): add curses.error handler

--- games_of_terminal/scripts/app.py
+++ games_of_terminal/scripts/app.py
@@ -1,11 +1,17 @@
 from games_of_terminal.menu.core import Menu
 import curses
+import sys
 
 
 @curses.wrapper
 def main(canvas):
-    menu = Menu(canvas)
-    menu.run_menu_loop()
+    try:
+        menu = Menu(canvas)
+        menu.run_menu_loop()
+    except curses.error as app_error:
+        message = f'Oops! Error: {app_error}'
+        sys.stderr.write(message)
+        sys.exit(1)
 
 
 if __name__ == '__main__':
