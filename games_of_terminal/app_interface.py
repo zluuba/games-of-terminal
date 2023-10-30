20e400cff953f3ece78a269d8196f0bb5957f184
zluuba <zluyba.nikitina@gmail.com>
2023-10-30 11:10:04 +0300

    feat: add func of drawing side menu tips

--- games_of_terminal/app_interface.py
+++ games_of_terminal/app_interface.py
@@ -1,6 +1,6 @@
 from games_of_terminal.colors import Colors
 from games_of_terminal.constants import (
-    LOGO, APP_NAME,
+    LOGO, APP_NAME, SIDE_MENU_TIPS,
 )
 import curses
 
@@ -15,8 +15,7 @@ class InterfaceManager(Colors):
     def _setup(self):
         self.height, self.width = self.canvas.getmaxyx()
 
-        canvas_bg_color = self.get_color_by_name('white_text_black_bg')
-        self.canvas.bkgd(' ', canvas_bg_color)
+        self.canvas.bkgd(' ', self.default_color)
 
         self._set_window_sizes()
         self._draw_main_window()
@@ -47,16 +46,26 @@ class InterfaceManager(Colors):
 
     def _setup_side_menu(self):
         self._draw_logo()
-        # self._draw_tips
+        self.draw_side_menu_tips()
 
     def _draw_logo(self):
-        color = self.get_color_by_name('white_text_black_bg')
-
         for y, line in enumerate(LOGO, start=1):
-            self.draw_message(y, 2, self.logo_box, line, color)
+            self.draw_message(y, 2, self.logo_box, line, self.default_color)
 
         y, x = 6, (self.logo_box_width - len(APP_NAME)) // 2
-        self.draw_message(y, x, self.logo_box, APP_NAME, color)
+        self.draw_message(y, x, self.logo_box, APP_NAME, self.default_color)
+
+    def draw_side_menu_tips(self, y=2, x=2, tips=SIDE_MENU_TIPS):
+        for key, description in tips.items():
+            self._clear_line(y, x, self.side_menu_box, (self.side_menu_box_width - (x * 2)))
+
+            message = f'{key} - {description}'
+            self.draw_message(y, x, self.side_menu_box, message, self.default_color)
+            y += 1
+
+    def _clear_line(self, y, x, field, width):
+        empty_line = ' ' * width
+        self.draw_message(y, x, field, empty_line, self.default_color)
 
     @staticmethod
     def draw_message(y, x, field, message, color):
