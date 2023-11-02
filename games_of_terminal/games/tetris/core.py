1f147c87b167327003e22ae94a846d6b80f0ff30
zluuba <zluyba.nikitina@gmail.com>
2023-11-02 17:23:48 +0300

    feat(tetris): add block constraction, block falling logic

--- games_of_terminal/games/tetris/core.py
+++ games_of_terminal/games/tetris/core.py
@@ -1,13 +1,15 @@
-from games_of_terminal.constants import KEYS, MESSAGES
+from games_of_terminal.constants import KEYS
 from games_of_terminal.games.engine import GameEngine
 from games_of_terminal.games.tetris.cell import TetrisCell
+from games_of_terminal.games.tetris.block import TetrisBlock
 
 from games_of_terminal.games.tetris.constants import (
-    BLOCKS, DIRECTIONS, FLIP_BLOCK, DOWN,
+    BLOCKS, DIRECTIONS, FLIP_BLOCK,
     CELL_WIDTH, CELL_HEIGHT,
 )
 
-from curses import endwin, flash
+from curses import endwin
+from time import time
 from random import choice
 
 
@@ -18,74 +20,72 @@ class TetrisGame(GameEngine):
         self.score = 0
         self.cells = {}
 
-        self.block = []
-        self.block_coordinates = None
+        self.block = None
+        self.falling_direction = 'down'
+
+        self.time_interval = 1
+        self.time = time()
 
     def _setup_game_field(self):
         self.hide_cursor()
         self.window.nodelay(1)
-        self.window.timeout(150)
 
         self._draw_game_field()
         self._setup_side_menu()
         self.show_game_status()
 
-    def _place_new_block(self):
-        shape_color = self.get_random_colored_background_color()
-
+    def _add_new_block_on_field(self):
         block_shape_name = choice(list(BLOCKS))
-        block_shape = BLOCKS[block_shape_name]
-
-        block_height = len(block_shape)
-        block_width = len(block_shape[0])
+        self.block = TetrisBlock(block_shape_name)
 
         y = self.game_area.begin_y + 1
-        x = begin_x = (self.game_area.width - 2) // 2 - block_width // 2
+        x = begin_x = (self.game_area.width - 2) // 2 - self.block.width // 2
 
-        self.block_coordinates = (y, x)
+        self.block.coordinates = (y, x)
 
-        for i in range(block_height):
-            for j in range(block_width):
+        for i in range(self.block.height):
+            for j in range(self.block.width):
                 cell = self.cells[(y, x)]
+
+                if self.block.blueprint[i][j]:
+                    self.block.add_cell(i, j, cell, self.block.name)
+                else:
+                    self.block.add_cell(i, j, cell)
+
                 x += CELL_WIDTH
-
-                if not block_shape[i][j]:
-                    continue
-
-                cell.owner = 'falling_block'
-                cell.block_color = shape_color
 
             y += CELL_HEIGHT
             x = begin_x
 
+        self.block.show()
+
     def start_new_game(self):
         self._setup_game_field()
-        self._place_new_block()
+        self._add_new_block_on_field()
+
+        self.time = time()
 
         while True:
             key = self.window.getch()
-            self.wait_for_keypress()
 
             if key == KEYS['escape']:
                 endwin()
                 return
 
             if key == FLIP_BLOCK:
-                self._flip_block(key)
+                self.block.flip()
             elif key in DIRECTIONS:
-                self._move_block(key)
+                direction = DIRECTIONS[key]
+                self.block.move(direction, self.cells)
 
-            # self._move_block('down')
+            self._block_auto_move()
 
-    def _move_block(self, direction):
-        if direction == 'down':
-            y, x = self.block_coordinates
-            self._clear_line(y, x, self.game_area.box, 4)
+    def _block_auto_move(self):
+        current_time = time()
 
-            self.block_coordinates = y + 1, x
-
-    def _flip_block(self, direction):
-        pass
+        if current_time - self.time >= self.time_interval:
+            self.block.move(self.falling_direction, self.cells)
+            self.time = current_time
 
     def _draw_game_field(self):
         lines = (self.game_area.height - 2)
@@ -107,5 +107,4 @@ class TetrisGame(GameEngine):
     def _create_cell(self, y, x):
         field_box = self.game_area.box.subwin(CELL_HEIGHT, CELL_WIDTH, y, x)
         cell = TetrisCell(field_box, (y, x))
-        cell.set_background_color()
         return cell
