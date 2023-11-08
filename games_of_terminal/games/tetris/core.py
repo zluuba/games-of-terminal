f019ddd35792a106eeacd3a26a58a0dcdeb420b5
zluuba <zluyba.nikitina@gmail.com>
2023-11-08 17:08:06 +0300

    fix(tetris): rebuild tetris core, add board logic insted of cells

--- games_of_terminal/games/tetris/core.py
+++ games_of_terminal/games/tetris/core.py
@@ -1,7 +1,7 @@
 from games_of_terminal.constants import KEYS
 from games_of_terminal.games.engine import GameEngine
-from games_of_terminal.games.tetris.cell import TetrisCell
 from games_of_terminal.games.tetris.block import TetrisBlock
+from games_of_terminal.games.tetris.game_board import TetrisBoard
 
 from games_of_terminal.games.tetris.constants import (
     BLOCKS, DIRECTIONS, FLIP_BLOCK,
@@ -17,51 +17,17 @@ class TetrisGame(GameEngine):
     def __init__(self, canvas):
         super().__init__(canvas)
 
-        self.score = 0
-        self.cells = {}
-
         self.block = None
+        self.board = None
         self.falling_direction = 'down'
 
+        self.score = 0
         self.time_interval = 1
         self.time = time()
 
-    def _setup_game_field(self):
-        self.hide_cursor()
-        self.window.nodelay(1)
-
-        self._draw_game_field()
-        self._setup_side_menu()
-        self.show_game_status()
-
-    def _add_new_block_on_field(self):
-        block_shape_name = choice(list(BLOCKS))
-        self.block = TetrisBlock(block_shape_name)
-
-        y = self.game_area.begin_y + 1
-        x = begin_x = (self.game_area.width - 2) // 2 - self.block.width // 2
-
-        self.block.coordinates = (y, x)
-
-        for i in range(self.block.height):
-            for j in range(self.block.width):
-                cell = self.cells[(y, x)]
-
-                if self.block.blueprint[i][j]:
-                    self.block.add_cell(i, j, cell, self.block.name)
-                else:
-                    self.block.add_cell(i, j, cell)
-
-                x += CELL_WIDTH
-
-            y += CELL_HEIGHT
-            x = begin_x
-
-        self.block.show()
-
     def start_new_game(self):
         self._setup_game_field()
-        self._add_new_block_on_field()
+        self.add_new_block_on_field()
 
         self.time = time()
 
@@ -76,35 +42,58 @@ class TetrisGame(GameEngine):
                 self.block.flip()
             elif key in DIRECTIONS:
                 direction = DIRECTIONS[key]
-                self.block.move(direction, self.cells)
+                self.block.move(direction)
 
             self._block_auto_move()
+
+            if self.is_block_on_floor():
+                self.board.land_block(self.block)
+                self.board.draw()
+                self.add_new_block_on_field()
+
+    def _setup_game_field(self):
+        self.hide_cursor()
+        self.window.nodelay(1)
+
+        self._setup_side_menu()
+        self.show_game_status()
+
+        self.board = TetrisBoard(self.game_area)
+
+    def add_new_block_on_field(self):
+        block_shape_name = choice(list(BLOCKS))
+        self.block = TetrisBlock(block_shape_name, self.game_area)
+
+        y = self.game_area.begin_y + 1
+        x = (self.game_area.width - 2) // 2 - self.block.width // 2
+
+        self.block.coordinates = (y, x)
+        self.block.draw()
 
     def _block_auto_move(self):
         current_time = time()
 
         if current_time - self.time >= self.time_interval:
-            self.block.move(self.falling_direction, self.cells)
+            self.block.move(self.falling_direction)
             self.time = current_time
 
-    def _draw_game_field(self):
-        lines = (self.game_area.height - 2)
-        cols = (self.game_area.width - 2)
+    def is_block_on_floor(self):
+        begin_y, begin_x = self.block.coordinates
 
-        y = self.game_area.begin_y + 1
-        x = begin_x = self.game_area.begin_x + 1
+        end_y = begin_y + (self.block.height * CELL_HEIGHT)
+        end_x = begin_x + (self.block.width * CELL_WIDTH)
 
-        for _ in range(lines):
-            for _ in range(cols):
-                cell = self._create_cell(y, x)
-                self.cells[(y, x)] = cell
+        for y in range(begin_y, end_y, CELL_HEIGHT):
+            for x in range(begin_x, end_x, CELL_WIDTH):
+                blueprint_y = (y - begin_y) // CELL_HEIGHT
+                blueprint_x = (x - begin_x) // CELL_WIDTH
+                blueprint_cell = self.block.blueprint[blueprint_y][blueprint_x]
 
-                x += 1
-
-            y += 1
-            x = begin_x
-
-    def _create_cell(self, y, x):
-        field_box = self.game_area.box.subwin(CELL_HEIGHT, CELL_WIDTH, y, x)
-        cell = TetrisCell(field_box, (y, x))
-        return cell
+                if blueprint_cell:
+                    end_y = y + CELL_HEIGHT
+                    end_x = x + CELL_WIDTH
+                    if (end_y, end_x) in self.board.box and self.board.box[(end_y, end_x)] == 'placed_block':
+                        return True
+                    if end_y >= self.game_area.bottom_border:
+                        return True
+        return False
