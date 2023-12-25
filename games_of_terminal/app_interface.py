46e1746447708884b94b7afa433234300c0d90da
zluuba <zluyba.nikitina@gmail.com>
2023-12-25 14:04:59 +0300

    feat: add win_too_small handler, refactor

--- games_of_terminal/app_interface.py
+++ games_of_terminal/app_interface.py
@@ -1,31 +1,27 @@
-from games_of_terminal.colors import Colors
-from games_of_terminal.field import Field
-from games_of_terminal.constants import (
-    LOGO, APP_NAME, SIDE_MENU_TIPS, STATUS_BOX_HEIGHT,
-    BASE_OFFSET, DEFAULT_YX_OFFSET,
-)
-
 from curses import newwin, curs_set
+from time import sleep
+
+from .colors import Colors
+from .field import Field
+from .constants import *
 
 
 class InterfaceManager(Colors):
     def __init__(self, canvas):
         super().__init__()
 
-        self.height, self.width = canvas.getmaxyx()
         self.canvas = canvas
         self._setup()
 
     def _setup(self):
+        self.height, self.width = self.canvas.getmaxyx()
         self.canvas.bkgd(' ', self.default_color)
-
         self._set_window_sizes()
         self._init_main_window()
         self._init_subwindows()
 
     def _init_main_window(self):
         window_sizes = self.window_box_sizes.values()
-
         self.window = newwin(*window_sizes)
         self.window.nodelay(True)
         self.window.keypad(True)
@@ -33,40 +29,16 @@ class InterfaceManager(Colors):
     def _init_subwindows(self):
         self.game_area = Field(self.window, *self.game_box_sizes.values())
         self.side_menu = Field(self.window, *self.side_menu_box_sizes.values())
-
         self.tips_area = Field(self.side_menu.box, *self.tips_box_sizes.values())
         self.logo_area = Field(self.side_menu.box, *self.logo_box_sizes.values())
         self.game_status_area = Field(self.side_menu.box, *self.status_box_sizes.values())
 
-    def setup_side_menu(self):
-        self._draw_logo()
-        self.draw_side_menu_tips()
-
-    def _draw_logo(self):
+    def draw_logo(self):
         for y, line in enumerate(LOGO, start=1):
             self.draw_message(y, 2, self.logo_area.box, line, self.default_color)
 
         y, x = 6, (self.logo_area.width - len(APP_NAME)) // 2
         self.draw_message(y, x, self.logo_area.box, APP_NAME, self.default_color)
-
-    def draw_side_menu_tips(self, y=2, x=2, tips=None, color=None):
-        if tips is None:
-            tips = SIDE_MENU_TIPS
-
-        if color is None:
-            color = self.default_color
-
-        for key, description in tips.items():
-            self._clear_line(y, x, self.tips_area.box, (self.tips_area.width - (x * 2)))
-
-            message = f'{key} - {description}'
-            self.draw_message(y, x, self.tips_area.box, message, color)
-            y += 1
-
-    def _clear_line(self, begin_y, begin_x, field, width):
-        empty_line = ' ' * width
-        self.draw_message(begin_y, begin_x, field,
-                          empty_line, self.default_color)
 
     @staticmethod
     def draw_message(begin_y, begin_x, field, message, color):
@@ -81,8 +53,36 @@ class InterfaceManager(Colors):
         self.window.timeout(-1)
 
     def redraw_window(self):
+        self.win_too_small_handle()
         self.window.clear()
-        self._setup()
+        self.__init__(self.canvas)
+
+    def is_window_too_small(self):
+        return (self.height < MIN_WIN_HEIGHT or
+                self.width < MIN_WIN_WIDTH)
+
+    def win_too_small_handle(self):
+        while self.is_window_too_small():
+            self.show_win_too_small_msg()
+            sleep(0.3)
+
+            key = self.canvas.getch()
+            self.wait_for_keypress()
+
+            if key == KEYS['resize']:
+                self.height, self.width = self.canvas.getmaxyx()
+
+    def show_win_too_small_msg(self):
+        message = 'Window is too small.'
+        message_length = len(message)
+
+        if message_length < self.width:
+            return
+
+        y = self.height // 2
+        x = (self.width // 2) - (message_length // 2)
+        self.draw_message(y, x, self.canvas, message, self.default_color)
+        self.canvas.refresh()
 
     def _set_window_sizes(self):
         begin_x = begin_y = 0
