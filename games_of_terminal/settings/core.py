d2ca9f577def05f61e91cfe177a23f3d400b8c0d
zluuba <zluyba.nikitina@gmail.com>
2023-12-26 10:48:17 +0300

    feat(settings): add open_selected_settings, refactor

--- games_of_terminal/settings/core.py
+++ games_of_terminal/settings/core.py
@@ -17,6 +17,11 @@ class Settings(InterfaceManager):
         self.title_start_y = (self.height // 2) - ((len(TITLE) + len(ITEMS)) // 2) - 3
         self.items_start_y = self.title_start_y + len(TITLE) + 3
 
+        self.noise_chars = ['.', '-', '-', '_', '_', '|', '|']
+        self.noise_chars_len = len(self.noise_chars)
+        self.noise_color = self.get_color_by_name('grey_text_black_bg')
+        self.chars_per_line = self.width // 4
+
     def run(self):
         self.initialize_settings()
 
@@ -30,24 +35,28 @@ class Settings(InterfaceManager):
                 self.move_menu_selection(-1)
             elif key in (KEYS['down_arrow'], KEYS['s']):
                 self.move_menu_selection(1)
+            elif key in KEYS['enter']:
+                self.open_selected_settings()
 
             self.update_menu_display()
             self.window.refresh()
 
+    def open_selected_settings(self):
+        chosen_settings = ITEMS[self.current_row]
+        settings_name = chosen_settings['name']
+        settings_class = chosen_settings['class']
+
+        settings = settings_class(self.canvas, settings_name)
+        settings.run()
+
     def initialize_settings(self):
         self.window.clear()
         self.hide_cursor()
-        self.draw_noise_animation()
-        self.draw_settings()
 
-    def draw_settings(self):
-        # draw static parts
-        self.show_settings_title()
-        self.show_items_list()
+        self.update_menu_display()
         self.window.refresh()
 
     def update_menu_display(self):
-        # redraw dynamic parts
         self.draw_noise_animation()
         self.show_settings_title()
         self.show_items_list()
@@ -60,17 +69,21 @@ class Settings(InterfaceManager):
     def show_items_list(self):
         begin_y = self.items_start_y
 
-        for row, game in enumerate(ITEMS.values()):
-            game_name = game['name']
+        for row, settings in enumerate(ITEMS.values()):
+            settings_name = settings['name']
 
-            begin_y += 1 if game_name == 'Reset All' else 0
+            begin_y += 1 if settings_name == 'Reset All' else 0
             y = begin_y + row
-            x = (self.width // 2) - (len(game_name) // 2)
+            x = (self.width // 2) - (len(settings_name) // 2)
 
-            color = self.default_color + REVERSE if row == self.current_row \
-                else self.default_color
+            if row == self.current_row:
+                color = self.default_color + REVERSE
+            elif settings['status'] == 'in_development':
+                color = self.get_color_by_name('grey_text_black_bg')
+            else:
+                color = self.default_color
 
-            self.draw_message(y, x, self.window, game_name, color)
+            self.draw_message(y, x, self.window, settings_name, color)
 
     def move_menu_selection(self, direction):
         self.current_row = max(
@@ -78,20 +91,21 @@ class Settings(InterfaceManager):
         )
 
     def draw_noise_animation(self):
-        self.window.erase()
+        self.window.clear()
 
-        chars = [' ', '.', '-', '-', '-', '|', '/', 'L']
-        last_char_num = len(chars)
+        curr_char_num = self.noise_chars_len
+        start = self.height - 1
+        end = self.height - 1 - self.noise_chars_len
 
-        for _ in range(self.height - 1, self.height - 1 - len(chars), -1):
-            for _ in range(self.width // 3):
+        for _ in range(start, end, -1):
+            for _ in range(self.chars_per_line):
                 y = randint(0, self.height - 1)
                 x = randint(0, self.width - 1)
-                color = self.get_color_by_name('grey_text_black_bg')
-                char = choice(chars[:last_char_num]) if last_char_num > 0 else ' '
 
-                if not (1 < x < self.width - 1):
+                if not (1 < x < self.width - 1) or curr_char_num <= 0:
                     continue
 
-                self.window.addstr(y, x, char, color)
-            last_char_num -= 1
+                char = choice(self.noise_chars[:curr_char_num])
+                self.window.addstr(y, x, char, self.noise_color)
+
+            curr_char_num -= 1
