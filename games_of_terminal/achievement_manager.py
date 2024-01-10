75a01d4a51786fe99f117697120be454dcc1b014
zluuba <zluyba.nikitina@gmail.com>
2024-01-10 19:32:42 +0300

    feat: add achievement manager

---
+++ games_of_terminal/achievement_manager.py
@@ -0,0 +1,12 @@
+class AchievementManager:
+    def snake_game_manager(self, game_state):
+        pass
+
+    def tetris_game_manager(self, game_state):
+        pass
+
+    def tictactoe_game_manager(self, game_state):
+        pass
+
+    def minesweeper_game_manager(self, game_state):
+        pass
