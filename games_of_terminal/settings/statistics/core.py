713b108b9b1e0677437c675e6549da1df8788eee
zluuba <zluyba.nikitina@gmail.com>
2024-01-11 17:00:48 +0300

    feat(statistics): add common stats, update logic

--- games_of_terminal/settings/statistics/core.py
+++ games_of_terminal/settings/statistics/core.py
@@ -9,6 +9,8 @@ from games_of_terminal.utils import (
     draw_message, get_color_by_name, clear_field_line,
 )
 
+from datetime import timedelta
+
 
 class Statistics(InterfaceManager):
     def __init__(self, canvas, settings_name):
@@ -17,11 +19,17 @@ class Statistics(InterfaceManager):
         self.settings_name = settings_name
 
         statistic_raw_data = get_games_statistic()
-        self.max_elem_width = self.get_max_elem_width(
+        statistic_raw_data = self.add_common_stats_into_statistics(
             statistic_raw_data
         )
-        self.statistics_data = self.get_statistics_prettify_data(
+        prettify_statistics_data = self.get_statistics_prettify_data(
             statistic_raw_data
+        )
+        self.max_elem_width = self.get_max_elem_width(
+            prettify_statistics_data
+        )
+        self.statistics_data = self.get_statistics_list_data(
+            prettify_statistics_data
         )
 
         self.logo_start_y = TOP_OFFSET
@@ -135,32 +143,80 @@ class Statistics(InterfaceManager):
         draw_elem_x = (self.width // 2) - (len(message) // 2)
         draw_message(y, draw_elem_x, self.window, message, color)
 
-    def get_statistics_prettify_data(self, statistics_dict):
-        statistics_prettify_data = []
+    def get_statistics_list_data(self, statistics_dict):
+        sorted_statistics = sorted(
+            statistics_dict.items(), key=lambda items: items[0],
+        )
+        statistics_list_data = []
 
-        for game_name, game_data in statistics_dict.items():
-            statistics_prettify_data.append(('visual_separation_line', ''))
-            statistics_prettify_data.append(('game_name', game_name))
+        for game_name, game_data in sorted_statistics:
+            if game_name:
+                statistics_list_data.append(('visual_separation_line', ''))
+                statistics_list_data.append(('game_name', game_name))
 
             for stat_name, stat_data in game_data.items():
-                stat_name, stat_data = str(stat_name), str(stat_data)
-                stat_name = self.get_normalized_stat_name(stat_name)
-                spaces = self.get_spaces_count(stat_name, stat_data)
-                prettify_stat = stat_name + ':' + (' ' * spaces) + stat_data
-                statistics_prettify_data.append(('stat', prettify_stat))
+                stat = self.get_filled_by_spaces_stat(stat_name, stat_data)
+                statistics_list_data.append(('stat', stat))
 
-        return statistics_prettify_data[1:]
+        return statistics_list_data
 
-    # def get_total_games_and_total_time(self, statistics_dict):
-    #     total_games = 0
-    #     total_time = 0
-    # 
-    #     for _, game_data in statistics_dict.items():
-    #         for stat_name, stat_data in game_data.items():
-    #             if stat_name == 'total_games':
-    #                 total_games += stat_data
-    #             elif stat_name == 'total_time':
-    #                 pass
+    def get_statistics_prettify_data(self, statistics_dict):
+        prettify_statistics_dict = dict()
+
+        for game_name, game_data in statistics_dict.items():
+            for stat_name, stat_data in game_data.items():
+                stat_name, stat_data = self.get_prettify_stats(
+                    stat_name, stat_data,
+                )
+
+                if game_name not in prettify_statistics_dict:
+                    prettify_statistics_dict[game_name] = {}
+
+                prettify_statistics_dict[game_name][stat_name] = stat_data
+
+        return prettify_statistics_dict
+
+    def get_prettify_stats(self, stat_name, stat_data):
+        if 'time' in stat_name:
+            stat_data = self.get_prettify_time(stat_data)
+
+        stat_name, stat_data = str(stat_name), str(stat_data)
+        stat_name = self.get_normalized_stat_name(stat_name)
+        return stat_name, stat_data
+
+    def get_filled_by_spaces_stat(self, stat_name, stat_data):
+        spaces = self.get_spaces_count(stat_name, stat_data)
+        prettify_stat = stat_name + ':' + (' ' * spaces) + stat_data
+        return prettify_stat
+
+    def get_total_games_and_total_time(self, statistics_dict):
+        total_games = 0
+        total_time = 0
+
+        for _, game_data in statistics_dict.items():
+            for stat_name, stat_data in game_data.items():
+                if stat_name == 'total_games':
+                    total_games += stat_data
+                elif stat_name == 'total_time':
+                    total_time += stat_data
+
+        return total_games, total_time
+
+    def add_common_stats_into_statistics(self, statistics_dict):
+        total_games, total_time = self.get_total_games_and_total_time(
+            statistics_dict
+        )
+
+        statistics_dict[''] = {
+            'all_games_played': total_games,
+            'time_spent_in_games': total_time,
+        }
+
+        return statistics_dict
+
+    @staticmethod
+    def get_prettify_time(time_in_seconds):
+        return str(timedelta(seconds=time_in_seconds))
 
     def get_spaces_count(self, *args):
         chars_count = sum(map(lambda word: len(word), args))
