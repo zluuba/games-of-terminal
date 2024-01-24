3888af02cc364eef4ca455b2becae8de414d6c2e
zluuba <zluyba.nikitina@gmail.com>
2024-01-24 17:52:06 +0300

    feat(db): rebuild all tables and queries

--- games_of_terminal/database/queries.py
+++ games_of_terminal/database/queries.py
@@ -1,24 +1,32 @@
 TABLES = {
-    'Games': '''
-        CREATE TABLE IF NOT EXISTS Games (
-            id INTEGER PRIMARY KEY,
-            game_name TEXT NOT NULL,
-            total_games INTEGER DEFAULT 0 NOT NULL,
-            total_time INTEGER DEFAULT 0 NOT NULL,
-            game_stats TEXT DEFAULT '',
-            last_game_state TEXT DEFAULT '',
-            settings TEXT DEFAULT ''
+    'game': '''
+        CREATE TABLE IF NOT EXISTS game (
+            id_game INTEGER PRIMARY KEY,
+            name TEXT NOT NULL
         );
     ''',
-    'Achievements': '''
-        CREATE TABLE IF NOT EXISTS Achievements (
-            id INTEGER PRIMARY KEY,
-            game_name TEXT NOT NULL,
-            name TEXT,
-            description TEXT,
+    # data_type - achievement, statistic, setting
+    'game_data': '''
+        CREATE TABLE IF NOT EXISTS game_data (
+            id_game_data INTEGER PRIMARY KEY,
+            id_game INTEGER,
+            data_type TEXT NOT NULL,
+            name TEXT NOT NULL,
+            value TEXT NOT NULL,
+            value_type TEXT NOT NULL,
+            FOREIGN KEY (game_id) REFERENCES game(id_game)
+        );
+    ''',
+    'achievement': '''
+        CREATE TABLE IF NOT EXISTS achievement (
+            id_achievement INTEGER PRIMARY KEY,
+            id_game INTEGER NULL,
+            name TEXT UNIQUE NOT NULL,
+            description TEXT NOT NULL,
             status TEXT DEFAULT 'locked',
-            date_received DATE DEFAULT NULL,
-            UNIQUE (name)
+            date_received TEXT NULL,
+            global INTEGER DEFAULT 0,
+            FOREIGN KEY (game_id) REFERENCES game(id_game)
         );
     ''',
 }
@@ -28,57 +36,55 @@ SELECT name FROM sqlite_master
 WHERE type='table';
 '''
 
-insert_or_ignore_achievement_query = '''
-INSERT OR IGNORE INTO Achievements (
-    game_name,
+insert_game_achievement_query = '''
+INSERT INTO achievement (
+    id_game,
     name,
     description
 )
 VALUES (?, ?, ?);
 '''
 
-insert_game_stats_query = '''
-INSERT INTO games (
-    game_name,
-    game_stats
+insert_global_achievement_query = '''
+INSERT INTO achievement (
+    name,
+    description,
+    global
 )
-VALUES (?, ?);
+VALUES (?, ?, ?);
 '''
 
-get_game_statistic_query = '''
-SELECT total_games, total_time, game_stats
-FROM Games
-WHERE game_name = ?;
+get_game_data_query = '''
+SELECT name, value, value_type
+FROM game_data
+WHERE id_game = ? AND data_type = ?;
 '''
 
 get_all_achievements_query = '''
-SELECT game_name, name, description, status, date_received
-FROM Achievements;
+SELECT 
+    game.name as game_name, 
+    achievement.name, 
+    achievement.description, 
+    achievement.status, 
+    achievement.date_received, 
+    achievement.global
+FROM achievement
+JOIN game ON achievement.id_game = game.id_game;
 '''
 
-update_achievements_table_query = """
-"""
+get_game_stat_query = '''
+SELECT game_data.value, game_data.value_type
+FROM game_data
+JOIN game ON game_data.id_game = game.id_game
+WHERE game.name = ? 
+    AND game_data.data_type = ? 
+    AND game_data.name = ?;
+'''
 
-
-def get_game_stat_query(stat_name):
-    return f'''
-        SELECT {stat_name}
-        FROM Games
-        WHERE game_name = ?;
-    '''
-
-
-def set_game_stat_query(stat_name):
-    return f'''
-        UPDATE Games
-        SET {stat_name} = ?
-        WHERE game_name = ?;
-    '''
-
-
-def update_game_stat_query(stat_name):
-    return f'''
-        UPDATE Games
-        SET {stat_name} = {stat_name} + ?
-        WHERE game_name = ?;
-    '''
+set_game_stat_query = '''
+UPDATE game_data
+SET value = ?, value_type = ?
+WHERE id_game IN (SELECT id_game FROM game WHERE name = ?)
+    AND data_type = ?
+    AND name = ?;
+'''
