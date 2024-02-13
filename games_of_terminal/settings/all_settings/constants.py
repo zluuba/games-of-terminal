4be4565dbee771bc19b0e98f84ef0fc888e737a8
zluuba <zluyba.nikitina@gmail.com>
2024-02-13 11:12:15 +0300

    feat(all_settings): add username constants

--- games_of_terminal/settings/all_settings/constants.py
+++ games_of_terminal/settings/all_settings/constants.py
@@ -1,3 +1,7 @@
+from string import ascii_letters, digits, punctuation
+from random import choice
+
+
 TITLE = [
     '╔═╗╔═╗╔╦╗╔╦╗╦╔╗╔╔═╗╔═╗',
     '╚═╗║╣  ║  ║ ║║║║║ ╗╚═╗',
@@ -13,3 +17,27 @@ NO_ARROW = ' '
 TOP_OFFSET = 2
 BOTTOM_OFFSET = 2
 SIDE_ARROW_OFFSET = 4
+
+USERNAME_EDITING_MSGS = (
+    'Enter new username and press Enter.',
+    'To cancel editing and go back, press Esc.',
+)
+
+USERNAME_INVALID_MSGS = (
+    'The username must contain at least one character.',
+    'The changes have not been applied.',
+)
+
+USERNAME_VALID_MSGS = (
+    '<USERNAME>? Is that your real name? Lmao',
+    'Changes have been applied.',
+    'Henceforth I name you <USERNAME>.',
+    'Hi, <USERNAME>. How you doin\'? ;)',
+    'Interesting choice.',
+    'Username with Twitter vibes, nice work.',
+    'So, you really want to be called <USERNAME>? Okay..',
+    'Username <<USERNAME>> was successfully saved.',
+)
+
+USERNAME_ALLOWED_CHARS = ascii_letters + digits + punctuation
+USERNAME_COLOR_NAME = 'strong_pastel_purple_text_black_bg'
