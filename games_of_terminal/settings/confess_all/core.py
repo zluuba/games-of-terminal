03051c41f943e0f6e5e90b98f46b26fd9995c0a6
zluuba <zluyba.nikitina@gmail.com>
2024-03-04 13:08:17 +0300

    feat(confess_all): add achievement manager

--- games_of_terminal/settings/confess_all/core.py
+++ games_of_terminal/settings/confess_all/core.py
@@ -1,3 +1,6 @@
+from games_of_terminal.achievements_manager import (
+    AchievementsManager,
+)
 from games_of_terminal.interface_manager import InterfaceManager
 from games_of_terminal.utils import show_placeholder_stub
 
@@ -12,3 +15,9 @@ class ConfessAll(InterfaceManager):
     def run(self):
         show_placeholder_stub(self.height, self.width, self.window)
         time.sleep(1)
+
+        achievement_manager = AchievementsManager(self, in_game=False)
+        achievement_manager.check(confess_option=True)
+
+    def draw_game_window(self):
+        pass
