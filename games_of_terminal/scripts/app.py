9b319c2b931460450f14a3e5297f33164f162c5b
zluuba <zluyba.nikitina@gmail.com>
2024-03-27 10:30:42 +0300

    feat(script): add bash colors setter (xterm-256)

--- games_of_terminal/scripts/app.py
+++ games_of_terminal/scripts/app.py
@@ -1,22 +1,32 @@
-from games_of_terminal.constants import TERM_RED_COLOR, ERROR_MSG
+from games_of_terminal.constants import (
+    TERM_DEFAULT_COLOR, TERM_RED_COLOR, ERROR_MSG,
+)
 from games_of_terminal.menu.core import Menu
 from games_of_terminal.utils import init_curses_colors
 
 from curses import (wrapper as curses_wrapper,
-                    error as curses_error)
+                    error as curses_error,
+                    has_colors, COLORS)
 from sys import exit
+from subprocess import run as run_bash_cmd
 
 
 @curses_wrapper
 def main(canvas):
+    if not has_colors():
+        run_bash_cmd('TERM=xterm-256color', shell=True)
+
     try:
         init_curses_colors()
         menu = Menu(canvas)
         menu.run_menu_loop()
         exit(0)
     except (curses_error, Exception) as error:
-        message = ERROR_MSG + '\n' + TERM_RED_COLOR + f'Error: {error}'
+        message = (ERROR_MSG + '\n' + 
+                   TERM_RED_COLOR + f'{error.__class__}: {error}' +
+                   TERM_DEFAULT_COLOR)
         exit(message)
+        # raise error
 
 
 if __name__ == '__main__':
